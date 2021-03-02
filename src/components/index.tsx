39acf07da5b63b9189b8a55a571fff2cc5f7b822
kyle <kylejeske@users.noreply.github.com>
2021-03-02 02:32:03 -0500

    Initial Commit (yarn, TypeScript, React, Webpack) Bare Boilerplate

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { App } from "./App";
+ReactDOM.render(
+  <App userName="2nx2b1" lang="TypeScript" />,
+  document.getElementById("output")
+);
